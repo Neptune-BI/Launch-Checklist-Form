adaad048582b1aeaa3da8e804c4a288ef5bece21
Neptune-BI <contact@neptune-BI.com>
2019-12-24 18:27:42 -0600

    Updated comments

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ window.addEventListener("load", function() {
          alert("All fields are required!");
          items.style.visibility = 'hidden';
       if (!isNaN(pilotName) || !isNaN(copilotName) || isNaN(fuelLevel) || isNaN(cargoMass) ){
-         alert("Use letters for names and digits for numbers.");
+         alert("Make sure to enter valid information for each field!");
          items.style.visibility = 'hidden';
          launchStatus.style.color = 'black';
 			launchStatus.innerHTML = 'Awaiting Information Before Launch';
@@ -82,14 +82,3 @@ function retrieveData() {
 	})
 
 }
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
